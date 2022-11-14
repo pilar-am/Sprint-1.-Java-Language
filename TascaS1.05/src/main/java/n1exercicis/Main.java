be6ec448978ef9a2c4542812aac549a7e66f07b0
pilar-am <71904337+pilar-am@users.noreply.github.com>
2022-11-14 12:12:19 +0100

    Update Main.java

--- TascaS1.05/src/main/java/n1exercicis/Main.java
+++ TascaS1.05/src/main/java/n1exercicis/Main.java
@@ -18,51 +18,63 @@ public class Main {
 
 	public static void main(String[] args) throws FileNotFoundException {
 
-		//Exercici 1
+		//Exercici 1  Classe que llisti alfabèticament el contingut d'un directori rebut per paràmetre.
+		System.out.println("LLISTAR ALFABÈTICAMENT CONTINGUT D'UN DIRECTORI");
+		
 		Path dir = Paths.get("C:\\Users\\prueba\\Documents\\EclipseWorkSpace\\Directorio");
 
+		Path dir2 = Paths.get(args[0]);
 		LecturaEscrituraDirectoris.llegirDirectori(dir);
+		LecturaEscrituraDirectoris.llegirDirectori(dir2);
 		
+		//Exercici 2  Llistar un arbre de directoris amb el contingut de tots els seus nivells (recursivament)
+		//Imprimir en pantalla en ordre alfabètic dins de cada nivell, indicant si és un directori (D) 
+		//o un fitxer (F), i la seva última data de modificació.
+		System.out.println("\nLLISTAR ALFABÈTICAMENT CONTINGUT D'UN DIRECTORI RECURSIVAMENT");
 		
-		//Exercici 2
-	    File directori = new File("C:\\Users\\prueba\\Documents\\EclipseWorkSpace\\Directorio");
-	    LecturaEscrituraDirectoris.fitxersRecursius(directori, "\t");
+		File directori = new File("C:\\Users\\prueba\\Documents\\EclipseWorkSpace\\Directorio");
+		File directori2 = new File(args[0]);
 	    
+	    	LecturaEscrituraDirectoris.fitxersRecursius(directori, "\t");
+	    	LecturaEscrituraDirectoris.fitxersRecursius(directori2, "\t");
 	    
-	    //Exercici 3
-		//File directori = new File("C:\\Users\\prueba\\Documents\\EclipseWorkSpace\\Directorio");
+	    	//Exercici 3  Guardar les dades a un fitxer.txt
 		FileWriter fitxer = null;
+		FileWriter fitxer3 = null;
 		try {
 			fitxer = new FileWriter("C:\\Users\\prueba\\Documents\\EclipseWorkSpace\\Directorio\\fitxer.txt");
+			fitxer3 = new FileWriter(args[1]);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		LecturaEscrituraDirectoris.escriureFitxer(directori, fitxer,"\t");
+		LecturaEscrituraDirectoris.escriureFitxer(directori2, fitxer,"\t");
 		
-		
-		//Exercici 4
-	    File arxiu = new File ("C:\\Users\\prueba\\Documents\\EclipseWorkSpace\\Directorio\\quijote.txt");;
-	    Charset charset = Charset.forName("UTF-8");
-	    LecturaEscrituraDirectoris.llegirFitxersTxt(arxiu, charset);
+		//Exercici 4  Llegir fitxers.txt
+		System.out.println("\nLECTURA D'UN FITXER TXT");
+	    	File arxiu = new File ("C:\\Users\\prueba\\Documents\\EclipseWorkSpace\\Directorio\\quijote.txt");
+	    	File arxiu2 = new File (args[3]);
+	    
+	    	Charset charset = Charset.forName("UTF-8");
+	    	LecturaEscrituraDirectoris.llegirFitxersTxt(arxiu, charset);
+	    	LecturaEscrituraDirectoris.llegirFitxersTxt(arxiu2, charset);  
 	      
-	      
-	    //Exercici 5 
-	    FileOutputStream fitxer2 = new FileOutputStream("fitxer.ser");
+	    	//Exercici 5 serialitzar un Objecte Java a un fitxer .ser i després l’ha de desserialitzar.
+	    	FileOutputStream fitxer2 = new FileOutputStream("fitxer.ser");
+	    	FileOutputStream fitxer4 = new FileOutputStream(args[4]);
 	    
-        //Data com a objecte
-        Date data = new Date();
+        	//Data com a objecte
+        	Date data = new Date();
         
-        SerialitzarDeserialitzar serialitzarDeserialitzar = new SerialitzarDeserialitzar();
+        	SerialitzarDeserialitzar serialitzarDeserialitzar = new SerialitzarDeserialitzar();
         
-        serialitzarDeserialitzar.serialitzar(fitxer2, data);
+        	serialitzarDeserialitzar.serialitzar(fitxer2, data);
+        	serialitzarDeserialitzar.serialitzar(fitxer4, data);
 	    
-        FileInputStream fitxerSer = new FileInputStream("fitxer.ser");
+        	FileInputStream fitxerSer = new FileInputStream("fitxer.ser");
         
-        serialitzarDeserialitzar.deserialitzar(fitxerSer);
-        
-	    
-	    
+        	serialitzarDeserialitzar.deserialitzar(fitxerSer);
 	}
 
 }
